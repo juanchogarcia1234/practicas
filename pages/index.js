e27488f330f8f73c2765990d5d3bb9555e966873
GarciaRamosJua <Juan.GarciaRamos.ext@hermesworld.com>
2021-08-27 14:39:12 +0300

    change lowercase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/Image";
+import Image from "next/image";
 import Head from "next/head";
 import Layout from "../components/layout";
 import Calendar from "../components/calendar";
