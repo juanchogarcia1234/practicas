ea1d9bdba8723dccec26e2f7120a3783f19eeae5
GarciaRamosJua <Juan.GarciaRamos.ext@hermesworld.com>
2021-09-11 12:54:51 +0300

    log

--- components/loginform.js
+++ components/loginform.js
@@ -14,7 +14,7 @@ export default function LoginForm() {
   async function submitFormHandler(event, email, password) {
     event.preventDefault();
     const result = await signIn("credentials", {
-      redirect: "/",
+      callbackUrl: "http://localhost:3000/foo",
       email,
       password
     });
