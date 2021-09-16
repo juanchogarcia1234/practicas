d63bc05450b1d9941717ee440b224ee78a40e3fe
GarciaRamosJua <Juan.GarciaRamos.ext@hermesworld.com>
2021-09-16 11:15:30 +0300

    another fix

--- pages/uroki/otmenennye.js
+++ pages/uroki/otmenennye.js
@@ -4,20 +4,15 @@ import Spinner from "../../components/spinner";
 import { useState, useEffect } from "react";
 
 export default function Cancelled() {
-  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     getSession().then(session => {
       if (!session) {
         window.location.href = "/login";
       } else {
-        setIsLoading(false);
       }
     });
   }, []);
 
-  if (isLoading) {
-    return <Spinner></Spinner>;
-  }
   return (
     <Layout>
       <>Cancelled</>
--- pages/uroki/perenesennye.js
+++ pages/uroki/perenesennye.js
@@ -4,20 +4,14 @@ import Spinner from "../../components/spinner";
 import { useState, useEffect } from "react";
 
 export default function Moved() {
-  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     getSession().then(session => {
       if (!session) {
         window.location.href = "/login";
-      } else {
-        setIsLoading(false);
       }
     });
   }, []);
 
-  if (isLoading) {
-    return <Spinner></Spinner>;
-  }
   return (
     <Layout>
       <>Moved</>
