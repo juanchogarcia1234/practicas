9f0027d33d42f2401fb0ea4968a40c403bdd068a
GarciaRamosJua <Juan.GarciaRamos.ext@hermesworld.com>
2021-10-14 18:21:11 +0300

    logic for adding new class

--- helpers.js
+++ helpers.js
@@ -3,3 +3,6 @@ import { format } from "date-fns";
 export const formatDateFromDB = date => date.toString().substr(0, 10);
 export const formatDateToDB = date => format(new Date(date.toString().substr(0, 28)), "y LL dd");
 export const formatHour = hour => hour.substr(11, 5);
+export const isLastElement = (myArray, el) => {
+  return myArray[myArray.length - 1] === el ? true : false;
+};
--- pages/api/classes.js
+++ pages/api/classes.js
@@ -1,5 +1,6 @@
 import { getSession } from "next-auth/client";
 import { connectToDatabase } from "../../lib/db";
+import { isLastElement } from "../../helpers";
 const ObjectId = require("mongodb").ObjectId;
 
 export default async function handler(req, res) {
@@ -28,13 +29,23 @@ export default async function handler(req, res) {
     } else if (action === "cancel") {
       //1º cancel class and get number of class to cancel
       classes = await classesCollection.updateOne({ _id: good_id }, { $set: { cancelled: true } });
-      let classToCancellNumber = await classesCollection.find({ _id: good_id }).toArray();
-      classToCancellNumber = classToCancellNumber[0].number;
+      let classToCancell = await classesCollection.find({ _id: good_id }).toArray();
+      let classToCancellNumber = classToCancell[0].number;
       //2º update rest of classes
       classes = await classesCollection.updateMany({ student: student, course: classCourse, number: { $gt: classNumber } }, { $inc: { number: -1 } });
-      //3º A) if cancelled class its the last
+      //3º Cojo los dias de la semana del curso
+      const coursesCollection = client.db().collection("courses");
+      let classDays = await coursesCollection.find({ number: classCourse, student: student }).toArray();
+      classDays = classDays[0].days;
+      //3A) Cojo la ultima clase para averiguar que día es y lo comparo con el array de días
       if (classToCancellNumber === 8) {
+        console.log(classDays.indexOf(classToCancell[0].day + 1));
       } else {
+        let lastClass = await classesCollection.find({ number: 7, student: student, course: classCourse }).toArray();
+        lastClass = lastClass[0];
+        let dayOfTheNewClass = isLastElement(classDays, lastClass.day) ? classDays[0] : classDays[classDays.indexOf(lastClass.day) + 1];
+        console.log("es la ultima clase de la semana", isLastElement(classDays, lastClass.day));
+        console.log("la nueva clase va a ser este dia", dayOfTheNewClass);
       }
       //3º A) Last class will be number 7 now but if cancelled class was the last, then it will be 8
       //3º A) new class will be always number 8?
