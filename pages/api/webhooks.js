4f6c4b7eff7014232973d01bd80a331de078dc70
GarciaRamosJua <Juan.GarciaRamos.ext@hermesworld.com>
2021-12-16 19:20:39 +0200

    test

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,9 +1,9 @@
 import { connectToDatabase } from "../../lib/db";
 export default async function handler(req, res) {
-  if (req.method === "GET") {
+  if (req.method === "POST") {
     const client = await connectToDatabase();
     const usersCollection = client.db().collection("users");
-    await usersCollection.insertOne({ email: "perico" });
+    await usersCollection.insertOne({ email: req.body });
     client.close();
   }
 
