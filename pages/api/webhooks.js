f90aa25ab6e909f270761b2fd7a5f26358f603ad
GarciaRamosJua <Juan.GarciaRamos.ext@hermesworld.com>
2021-12-16 19:28:21 +0200

    test

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -3,7 +3,9 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const client = await connectToDatabase();
     const usersCollection = client.db().collection("users");
-    await usersCollection.insertOne({ email: "juanito" });
+    let event = JSON.parse(req.body);
+    var uuid = event.payload.object.uuid;
+    await usersCollection.insertOne({ email: uuid });
     client.close();
   }
 
