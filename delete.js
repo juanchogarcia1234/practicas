dcefae89827d041174e6a9d8140fd7e49ade84c1
GarciaRamosJua <Juan.GarciaRamos.ext@hermesworld.com>
2021-12-16 18:48:04 +0200

    test

---
+++ delete.js
@@ -0,0 +1,37 @@
+const { MongoClient } = require("mongodb");
+
+async function main() {
+  // we'll add code here soon
+  const uri = "mongodb+srv://m001-student:m001-mongodb-basics@cluster0.jyaw3.mongodb.net/uroki?retryWrites=true&w=majority";
+
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+
+    // //Course collection
+    // const coursesCollection = await client.db().collection("courses");
+    // await coursesCollection.deleteOne({ number: 2, student: "Alexandr" });
+
+    //Clases collection
+    const classesCollection = await client.db().collection("classes");
+    await classesCollection.deleteMany({ student: "elmira" });
+  } catch (e) {
+    console.error(e);
+  } finally {
+    client.close();
+  }
+}
+
+main();
+
+// //Course collection
+// const coursesCollection = await client.db().collection("courses");
+// coursesCollection.deleteOne({ number: 2, student: "Alexandr" });
+
+// //Classes collection
+// const classesCollection = client.db().collection("classes");
+// classesCollection.deleteMany();
+
+// //Users collection
+// const usersCollection = client.db().collection("users");
---
+++ pages/api/webhooks.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    console.log(req);
+  }
+
+  res.status(200).json({ "hello" });
+}
