ede8c8acc90e733ed43d96678fd89a0fb5bb7336
GarciaRamosJua <Juan.GarciaRamos.ext@hermesworld.com>
2021-12-18 16:25:10 +0200

    date object to string

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,8 +1,15 @@
 import { connectToDatabase } from "../../lib/db";
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    console.log("funciona");
-    console.log(req);
+    //Añadir campo shareurl a la clase concreta
+    //Hacer un put a la clase con el nombre del alumno ("topic":"Clase con Alexandr") y la fecha ("recording_end":"2021-12-16T18:33:44Z") y añadir "share_url"
+    const client = await connectToDatabase();
+    const classesCollection = client.db().collection("classes");
+    const { topic, recording_end, share_url } = req.body.payload.object;
+
+    console.log("new Date", new Date(recording_end.substr(0, 10)).toString());
+
+    classes = await classesCollection.updateOne({ student: topic, start_time: recording_end.substring() }, { $set: { moved: true, start_time: addHours(parseISO(dateToMove), 3), end_time: addHours(parseISO(dateToMove), 4) } });
   }
   console.log(JSON.stringify(req.body.payload.object));
 
