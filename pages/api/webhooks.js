0fbf7ae1e8a7175a1bdafea5ec1d48b30ddaebe0
GarciaRamosJua <Juan.GarciaRamos.ext@hermesworld.com>
2021-12-18 16:38:29 +0200

    date object to string

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -8,7 +8,8 @@ export default async function handler(req, res) {
     const { topic, recording_end, share_url } = req.body.payload.object;
 
     console.log(recording_end);
-    console.log("new Date", new Date(recording_end.substr(0, 10)).toString());
+    console.log("hello");
+    // console.log("new Date", new Date(recording_end.substr(0, 10)).toString());
 
     classes = await classesCollection.updateOne({ student: topic, start_time: recording_end.substring() }, { $set: { moved: true, start_time: addHours(parseISO(dateToMove), 3), end_time: addHours(parseISO(dateToMove), 4) } });
   }
